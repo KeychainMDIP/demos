6eb32f562ca7f2a52c701f8338b7d9cd48879b6e
David McFadzean <davidmc@gmail.com>
2025-10-24 15:35:47 -0400

    Simplified metadata view

--- dex-demo/client/src/components/ViewAssetMetadata.tsx
+++ dex-demo/client/src/components/ViewAssetMetadata.tsx
@@ -1,7 +1,5 @@
-import React, { useEffect, useState } from "react";
-import { useNavigate, useParams } from "react-router-dom";
+import React from "react";
 import {
-    Button,
     Table,
     TableBody,
     TableContainer,
@@ -12,117 +10,10 @@ import { formatDate } from "../utils.js";
 import UserBadge from "./UserBadge.js";
 
 function ViewAssetMetadata({ asset }: { asset: any }) {
-    const { did } = useParams();
-    const navigate = useNavigate();
-    const [firstDid, setFirstDid] = useState<string | null>(null);
-    const [prevDid, setPrevDid] = useState<string | null>(null);
-    const [nextDid, setNextDid] = useState<string | null>(null);
-    const [lastDid, setLastDid] = useState<string | null>(null);
-
-    function findAdjacentDids() {
-        const list = asset.collection?.assets || [];
-
-        let prevDid = null;
-        let nextDid = null;
-        let firstDid = null;
-        let lastDid = null;
-
-        for (let i = 0; i < list.length; i++) {
-            if (list[i] === did) {
-                if (i > 0) {
-                    prevDid = list[i - 1];
-                }
-                if (i < list.length - 1) {
-                    nextDid = list[i + 1];
-                }
-                break;
-            }
-        }
-
-        firstDid = list[0];
-
-        if (firstDid === did) {
-            firstDid = null;
-        }
-
-        lastDid = list[list.length - 1];
-
-        if (lastDid === did) {
-            lastDid = null;
-        }
-
-        return { firstDid, prevDid, nextDid, lastDid };
-    }
-
-    useEffect(() => {
-        const init = async () => {
-            const { firstDid, prevDid, nextDid, lastDid } = findAdjacentDids();
-
-            setFirstDid(firstDid);
-            setPrevDid(prevDid);
-            setNextDid(nextDid);
-            setLastDid(lastDid);
-        };
-
-        init();
-    }, [asset]);
-
     return (
         <TableContainer>
             <Table>
                 <TableBody>
-                    <TableRow>
-                        <TableCell>Title</TableCell>
-                        {asset.token?.matrix ? (
-                            <TableCell><a href={`/asset/${asset.token?.matrix}`}>{asset.title || 'no title'}</a></TableCell>
-                        ) : (
-                            <TableCell>{asset.title || 'no title'}</TableCell>
-                        )}
-                    </TableRow>
-                    <TableRow>
-                        <TableCell>Collection</TableCell>
-                        {asset.collection?.name ? (
-                            <TableCell>
-                                <div style={{ display: 'inline-block' }}>
-                                    {!asset.token && (
-                                        <>
-                                            <Button
-                                                color="inherit"
-                                                disabled={!firstDid}
-                                                onClick={() => navigate(`/asset/${firstDid}`)}>
-                                                {'<<'}
-                                            </Button>
-                                            <Button
-                                                color="inherit"
-                                                disabled={!prevDid}
-                                                onClick={() => navigate(`/asset/${prevDid}`)}>
-                                                {'<'}
-                                            </Button>
-                                        </>
-                                    )}
-                                    <a href={`/collection/${asset.collection.did}`}>{asset.collection.name}</a>
-                                    {!asset.token && (
-                                        <>
-                                            <Button
-                                                color="inherit"
-                                                disabled={!nextDid}
-                                                onClick={() => navigate(`/asset/${nextDid}`)}>
-                                                {'>'}
-                                            </Button>
-                                            <Button
-                                                color="inherit"
-                                                disabled={!lastDid}
-                                                onClick={() => navigate(`/asset/${lastDid}`)}>
-                                                {'>>'}
-                                            </Button>
-                                        </>
-                                    )}
-                                </div>
-                            </TableCell>
-                        ) : (
-                            <TableCell>no collection</TableCell>
-                        )}
-                    </TableRow>
                     <TableRow>
                         <TableCell>Creator</TableCell>
                         {asset.creator ? (
